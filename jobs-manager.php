cadd244b2ea0cbc50ae5463dc5c49518e57924fd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-26 17:56:59 +0800

    Update jobs-manager.php

--- jobs-manager.php
+++ jobs-manager.php
@@ -12,7 +12,7 @@
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Donald Raymundo
- * Text Domain:       jobs-manager
+ * Text Domain:       jobsmanager
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
@@ -20,29 +20,29 @@
  /**
  * Register the "jobs" custom post type
  */
-function jobs_manager_setup_post_type() {
+function jobsmanager_setup_post_type() {
 	register_post_type( 'jobs', ['public' => true ] ); 
 } 
-add_action( 'init', 'jobs_manager_setup_post_type' );
+add_action( 'init', 'jobsmanager_setup_post_type' );
 
 /**
  * Activate the plugin.
  */
-function jobs_manager_activate() { 
+function jobsmanager_activate() { 
 	// Trigger our function that registers the custom post type plugin.
-	jobs_manager_setup_post_type(); 
+	jobsmanager_setup_post_type(); 
 	// Clear the permalinks after the post type has been registered.
 	flush_rewrite_rules(); 
 }
-register_activation_hook( __FILE__, 'jobs_manager_activate' );
+register_activation_hook( __FILE__, 'jobsmanager_activate' );
 
 /**
  * Deactivation hook.
  */
-function jobs_manager_deactivate() {
+function jobsmanager_deactivate() {
 	// Unregister the post type, so the rules are no longer in memory.
 	unregister_post_type( 'jobs' );
 	// Clear the permalinks to remove our post type's rules from the database.
 	flush_rewrite_rules();
 }
-register_deactivation_hook( __FILE__, 'jobs_manager_deactivate' );
+register_deactivation_hook( __FILE__, 'jobsmanager_deactivate' );
