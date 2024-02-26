fc8ab1a7ee08b8922c273b7a0e443a1d510db420
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-26 17:44:55 +0800

    Update jobs-manager.php

--- jobs-manager.php
+++ jobs-manager.php
@@ -7,11 +7,12 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Jobs Manager
+ * Description:       Jobs Manager by Digitally Disruptive
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            Jobs Manager
- * Text Domain:       jobs-maanger
+ * Author:            Donald Raymundo
+ * Text Domain:       jobs-manager
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
