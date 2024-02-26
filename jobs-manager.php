0cc5d0247ae924fb777d3c3ff46eca06b977c819
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-02-26 17:42:09 +0800

    Create jobs-manager.php

---
+++ jobs-manager.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Jobs Manager
+ *
+ * @author            Donald Raymundo
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Jobs Manager
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Jobs Manager
+ * Text Domain:       jobs-maanger
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
