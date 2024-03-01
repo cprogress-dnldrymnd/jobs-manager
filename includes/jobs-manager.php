bf0d2e616a9ae5fdfbca9d13b132d509562bb800
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 14:59:49 +0800

    css

--- includes/jobs-manager.php
+++ includes/jobs-manager.php
@@ -12,6 +12,93 @@ if (!class_exists('JobsManager')) {
             wp_enqueue_style('jobsmanager-style', plugin_dir_url(__FILE__) . 'jobsmanager.css');
             wp_enqueue_script('jobsmanager-style', plugin_dir_url(__FILE__) . 'jobsmanager.js');
         }
+
+
+
+        function get__post_meta($value)
+        {
+            if (function_exists('carbon_get_the_post_meta')) {
+                return carbon_get_the_post_meta($value);
+            } else {
+                return 'Error: Carbonfield not activated';
+            }
+        }
+
+        function get__term_meta($term_id, $value)
+        {
+            if (function_exists('carbon_get_term_meta')) {
+                return carbon_get_term_meta($term_id, $value);
+            } else {
+                return 'Error: Carbonfield not activated';
+            }
+        }
+
+        function get__post_meta_by_id($id, $value)
+        {
+            if (function_exists('carbon_get_post_meta')) {
+                return carbon_get_post_meta($id, $value);
+            } else {
+                return 'Error: Carbonfield not activated';
+            }
+        }
+        function get__theme_option($value)
+        {
+            if (function_exists('carbon_get_theme_option')) {
+                return carbon_get_theme_option($value);
+            } else {
+                return 'Error: Carbonfield not activated';
+            }
+        }
+
+        function disable_bootstrap()
+        {
+            return $this->get__theme_option('jobs_disable_bootstrap');
+        }
+
+
+        function jobs_single()
+        {
+            return $this->get__theme_option('jobs_single');
+        }
+
+        function jobs_slug()
+        {
+            return $this->get__theme_option('jobs_slug');
+        }
+
+        function jobs_alt_title()
+        {
+            return $this->get__theme_option('jobs_alt_title');
+        }
+
+        function jobs_description()
+        {
+            return $this->get__theme_option('jobs_description');
+        }
+
+        function get_terms_details($taxonomy, $hide_empty = false, $order = false)
+        {
+            $args = array(
+                'taxonomy' => $taxonomy,
+                'hide_empty' => $hide_empty,
+            );
+            if ($order) {
+                $args['meta_key'] = '_order';
+                $args['orderby'] = '_order';
+            }
+            $terms = get_terms($args);
+
+            if (!$terms) return;
+            $term_array = array();
+            foreach ($terms as $term) {
+                $term_array[$term->term_id] = array(
+                    'name' => $term->name,
+                    'short_description' => carbon_get_term_meta($term->term_id, 'short_description'),
+                    'icon' => carbon_get_term_meta($term->term_id, 'icon'),
+                );
+            }
+            return $term_array;
+        }
     }
 }
 
