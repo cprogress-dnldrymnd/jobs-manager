37790da6c8bfbb951bf730c2dedfc2f8ab160cdb
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 12:51:28 +0800

    Update jobs-manager.php

--- jobs-manager.php
+++ jobs-manager.php
@@ -18,9 +18,10 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
+require_once('includes/post-types.php');
 
 if (!class_exists('JobsManager')) {
-    class JobsManager
+    class JobsManager extends newPostType
     {
         var $api_key;
 
@@ -38,57 +39,17 @@ if (!class_exists('JobsManager')) {
         function jobsmanager_setup_post_type()
         {
 
-            $labels = array(
-                'name'                  => _x('Jobs', 'Job General Name', 'text_domain'),
-                'singular_name'         => _x('Job', 'Job Singular Name', 'text_domain'),
-                'menu_name'             => __('Jobs', 'text_domain'),
-                'name_admin_bar'        => __('Job', 'text_domain'),
-                'archives'              => __('Item Archives', 'text_domain'),
-                'attributes'            => __('Item Attributes', 'text_domain'),
-                'parent_item_colon'     => __('Parent Item:', 'text_domain'),
-                'all_items'             => __('All Items', 'text_domain'),
-                'add_new_item'          => __('Add New Item', 'text_domain'),
-                'add_new'               => __('Add New', 'text_domain'),
-                'new_item'              => __('New Item', 'text_domain'),
-                'edit_item'             => __('Edit Item', 'text_domain'),
-                'update_item'           => __('Update Item', 'text_domain'),
-                'view_item'             => __('View Item', 'text_domain'),
-                'view_items'            => __('View Items', 'text_domain'),
-                'search_items'          => __('Search Item', 'text_domain'),
-                'not_found'             => __('Not found', 'text_domain'),
-                'not_found_in_trash'    => __('Not found in Trash', 'text_domain'),
-                'featured_image'        => __('Featured Image', 'text_domain'),
-                'set_featured_image'    => __('Set featured image', 'text_domain'),
-                'remove_featured_image' => __('Remove featured image', 'text_domain'),
-                'use_featured_image'    => __('Use as featured image', 'text_domain'),
-                'insert_into_item'      => __('Insert into item', 'text_domain'),
-                'uploaded_to_this_item' => __('Uploaded to this item', 'text_domain'),
-                'items_list'            => __('Items list', 'text_domain'),
-                'items_list_navigation' => __('Items list navigation', 'text_domain'),
-                'filter_items_list'     => __('Filter items list', 'text_domain'),
-            );
-            $args = array(
-                'label'                 => __('Job', 'text_domain'),
-                'description'           => __('Job Description', 'text_domain'),
-                'labels'                => $labels,
-                'supports'              => false,
-                'hierarchical'          => false,
-                'public'                => true,
-                'show_ui'               => true,
-                'show_in_menu'          => true,
-                'menu_position'         => 5,
-                'show_in_admin_bar'     => true,
-                'show_in_nav_menus'     => true,
-                'can_export'            => true,
-                'has_archive'           => true,
-                'exclude_from_search'   => false,
-                'publicly_queryable'    => true,
-                'capability_type'       => 'page',
-            );
-            register_post_type('post_type', $args);
+            $Testimonials = new newPostType();
+            $Testimonials->name = 'Testimonials';
+            $Testimonials->singular_name = 'Testimonial';
+            $Testimonials->icon = 'dashicons-testimonial';
+            $Testimonials->supports = array('title', 'revisions');
+            $Testimonials->exclude_from_search = true;
+            $Testimonials->publicly_queryable = false;
+            $Testimonials->show_in_admin_bar = false;
+            $Testimonials->has_archive = false;
         }
     }
 }
-
 
 new JobsManager();
