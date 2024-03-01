2433b93bbef41cddd4128774c35b4b5e9a18befd
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 13:35:20 +0800

    cs

--- includes/jobs-manager.php
+++ includes/jobs-manager.php
@@ -5,7 +5,7 @@ if (!class_exists('JobsManager')) {
         function __construct()
         {
             add_action('wp_enqueue_scripts', array($this, 'assets'));
-            add_action('admin_menu', array($this, 'jobsmanager_setup_admin_page'));
+            //add_action('admin_menu', array($this, 'jobsmanager_setup_admin_page'));
         }
 
         function assets()
@@ -14,7 +14,7 @@ if (!class_exists('JobsManager')) {
             wp_enqueue_script('jobsmanager-style', plugin_dir_url(__FILE__) . 'jobsmanager.js');
         }
 
-
+        /*
         function jobsmanager_setup_admin_page()
         {
             add_menu_page('Job Manager', 'Job Manager', 'manage_options', 'jobsmanager', array($this, 'jobsmanager_setup_admin_page_init'));
@@ -23,7 +23,7 @@ if (!class_exists('JobsManager')) {
         function jobsmanager_setup_admin_page_init()
         {
             echo "<h1>Job Manager</h1>";
-        }
+        }*/
     }
 }
 
