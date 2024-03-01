084cd5de8628ccde43365b3de52f32f638cb4605
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 16:28:06 +0800

    Update ajax.php

--- includes/ajax.php
+++ includes/ajax.php
@@ -131,15 +131,25 @@ class Ajax
 										<div class="col">
 											<div class="button-box">
 												<a type="button" class="btn btn-link w-100" href="<?php the_permalink() ?>">
-													<span>LEARN MORE</span>
+													<span class="col text-center">LEARN MORE</span>
+													<span class="col-auto btn-icon">
+														<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+															<path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
+														</svg>
+													</span>
 												</a>
 											</div>
 										</div>
 									<?php } ?>
 									<div class="col">
 										<div class="button-box">
-											<button type="button" class="btn btn-primary w-100" data-title="<?php the_title() ?>" data-bs-toggle="modal" data-bs-target="#applyModal">
-												<span>APPLY FOR THIS POSITION</span><span class="arrow right white rotated"><span></span></span>
+											<button type="button" class="btn btn-primary w-100 d-flex" data-title="<?php the_title() ?>" data-bs-toggle="modal" data-bs-target="#applyModal">
+												<span class="col text-center">APPLY FOR THIS POSITION</span>
+												<span class="col-auto btn-icon">
+													<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+														<path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
+													</svg>
+												</span>
 											</button>
 										</div>
 									</div>
