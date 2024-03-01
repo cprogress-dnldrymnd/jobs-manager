fa09c80d342906b277e9d3a6f2604d6fdc9b6462
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 13:07:17 +0800

    cs

--- jobs-manager.php
+++ jobs-manager.php
@@ -18,7 +18,6 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-require_once('includes/post-types.php');
 
 if (!class_exists('JobsManager')) {
     class JobsManager
@@ -28,6 +27,7 @@ if (!class_exists('JobsManager')) {
         function __construct()
         {
             add_action('init', array($this, 'jobsmanager_setup_post_type'));
+            add_action('init', array($this, 'jobsmanager_setup_taxonomy'));
             add_action('wp_enqueue_scripts', array($this, 'assets'));
         }
 
@@ -39,26 +39,94 @@ if (!class_exists('JobsManager')) {
         function jobsmanager_setup_post_type()
         {
 
-            $Testimonials = new PostType();
-            $Testimonials->name = 'Testimonials';
-            $Testimonials->singular_name = 'Testimonial';
-            $Testimonials->icon = 'dashicons-testimonial';
-            $Testimonials->supports = array('title', 'revisions');
-            $Testimonials->exclude_from_search = true;
-            $Testimonials->publicly_queryable = false;
-            $Testimonials->show_in_admin_bar = false;
-            $Testimonials->has_archive = false;
+            $labels = array(
+                'name'                  => _x('Jobs', 'Job General Name', 'text_domain'),
+                'singular_name'         => _x('Job', 'Job Singular Name', 'text_domain'),
+                'menu_name'             => __('Jobs', 'text_domain'),
+                'name_admin_bar'        => __('Job', 'text_domain'),
+                'archives'              => __('Item Archives', 'text_domain'),
+                'attributes'            => __('Item Attributes', 'text_domain'),
+                'parent_item_colon'     => __('Parent Item:', 'text_domain'),
+                'all_items'             => __('All Items', 'text_domain'),
+                'add_new_item'          => __('Add New Item', 'text_domain'),
+                'add_new'               => __('Add New', 'text_domain'),
+                'new_item'              => __('New Item', 'text_domain'),
+                'edit_item'             => __('Edit Item', 'text_domain'),
+                'update_item'           => __('Update Item', 'text_domain'),
+                'view_item'             => __('View Item', 'text_domain'),
+                'view_items'            => __('View Items', 'text_domain'),
+                'search_items'          => __('Search Item', 'text_domain'),
+                'not_found'             => __('Not found', 'text_domain'),
+                'not_found_in_trash'    => __('Not found in Trash', 'text_domain'),
+                'featured_image'        => __('Featured Image', 'text_domain'),
+                'set_featured_image'    => __('Set featured image', 'text_domain'),
+                'remove_featured_image' => __('Remove featured image', 'text_domain'),
+                'use_featured_image'    => __('Use as featured image', 'text_domain'),
+                'insert_into_item'      => __('Insert into item', 'text_domain'),
+                'uploaded_to_this_item' => __('Uploaded to this item', 'text_domain'),
+                'items_list'            => __('Items list', 'text_domain'),
+                'items_list_navigation' => __('Items list navigation', 'text_domain'),
+                'filter_items_list'     => __('Filter items list', 'text_domain'),
+            );
+            $args = array(
+                'label'                 => __('Job', 'text_domain'),
+                'description'           => __('Job Description', 'text_domain'),
+                'labels'                => $labels,
+                'supports'              => false,
+                'hierarchical'          => false,
+                'public'                => true,
+                'show_ui'               => true,
+                'show_in_menu'          => true,
+                'menu_position'         => 5,
+                'show_in_admin_bar'     => true,
+                'show_in_nav_menus'     => true,
+                'can_export'            => true,
+                'has_archive'           => true,
+                'exclude_from_search'   => false,
+                'publicly_queryable'    => true,
+                'capability_type'       => 'page',
+            );
+            register_post_type('post_type', $args);
+        }
+
+        function jobsmanager_setup_taxonomy()
+        {
+
+
+            $labels = array(
+                'name'                       => _x('Locations', 'Location General Name', 'text_domain'),
+                'singular_name'              => _x('Location', 'Location Singular Name', 'text_domain'),
+                'menu_name'                  => __('Location', 'text_domain'),
+                'all_items'                  => __('All Items', 'text_domain'),
+                'parent_item'                => __('Parent Item', 'text_domain'),
+                'parent_item_colon'          => __('Parent Item:', 'text_domain'),
+                'new_item_name'              => __('New Item Name', 'text_domain'),
+                'add_new_item'               => __('Add New Item', 'text_domain'),
+                'edit_item'                  => __('Edit Item', 'text_domain'),
+                'update_item'                => __('Update Item', 'text_domain'),
+                'view_item'                  => __('View Item', 'text_domain'),
+                'separate_items_with_commas' => __('Separate items with commas', 'text_domain'),
+                'add_or_remove_items'        => __('Add or remove items', 'text_domain'),
+                'choose_from_most_used'      => __('Choose from the most used', 'text_domain'),
+                'popular_items'              => __('Popular Items', 'text_domain'),
+                'search_items'               => __('Search Items', 'text_domain'),
+                'not_found'                  => __('Not Found', 'text_domain'),
+                'no_terms'                   => __('No items', 'text_domain'),
+                'items_list'                 => __('Items list', 'text_domain'),
+                'items_list_navigation'      => __('Items list navigation', 'text_domain'),
+            );
+            $args = array(
+                'labels'                     => $labels,
+                'hierarchical'               => false,
+                'public'                     => true,
+                'show_ui'                    => true,
+                'show_admin_column'          => true,
+                'show_in_nav_menus'          => true,
+                'show_tagcloud'              => true,
+            );
+            register_taxonomy('Location', array('jobs'), $args);
         }
     }
 }
 
 new JobsManager();
-$Testimonials = new PostType();
-$Testimonials->name = 'Testimonials';
-$Testimonials->singular_name = 'Testimonial';
-$Testimonials->icon = 'dashicons-testimonial';
-$Testimonials->supports = array('title', 'revisions');
-$Testimonials->exclude_from_search = true;
-$Testimonials->publicly_queryable = false;
-$Testimonials->show_in_admin_bar = false;
-$Testimonials->has_archive = false;
