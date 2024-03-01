b1e6d13f9ff6f1b1671565d496e5c8f9debd5aa6
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 14:42:19 +0800

    css

--- includes/post-meta-fields.php
+++ includes/post-meta-fields.php
@@ -18,3 +18,24 @@ Container::make('theme_options', __('Settings'))
     ->add_tab('Contact Form', array(
         Field::make('text', 'jobs_contact_form', __('Jobs Contact Form Shortcode')),
     ));
+
+
+Container::make('post_meta', __('Job Details'))
+    ->where('post_type', '=', 'jobs')
+    ->add_fields(array(
+        Field::make('text', 'salary', 'Salary'),
+        Field::make('complex', 'accordion', 'Accordion')
+            ->add_fields(array(
+                Field::make('text', 'accordion_title', 'Accordion Title'),
+                Field::make('rich_text', 'accordion_content', 'Accordion Content'),
+            ))
+            ->set_default_value(array(
+                array(
+                    'accordion_title' => 'Responsibilites',
+                ),
+                array(
+                    'accordion_title' => 'Knowledge & Experience',
+                ),
+            ))
+            ->set_header_template('<%- accordion_title  %>')
+    ));
