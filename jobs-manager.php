1320e8dc194cc8ca8be4a666af732d5a6c47cf75
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 13:16:39 +0800

    Update jobs-manager.php

--- jobs-manager.php
+++ jobs-manager.php
@@ -135,7 +135,7 @@ if (!class_exists('JobsManager')) {
 
         function jobsmanager_setup_admin_page_init()
         {
-            echo "<h1>Hello World!</h1>";
+            echo "<h1>Job Manager</h1>";
         }
     }
 }
