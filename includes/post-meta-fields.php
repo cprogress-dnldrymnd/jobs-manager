251e58cda0519a5d98c64e5706878a438952b6c0
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 14:37:13 +0800

    Update post-meta-fields.php

--- includes/post-meta-fields.php
+++ includes/post-meta-fields.php
@@ -4,25 +4,10 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Complex_Container;
 use Carbon_Fields\Field;
 
-Container::make('theme_options', __('Settings'))
-    ->add_fields(
-        array(
-            Field::make('complex', 'our_schools', 'Our Schools')
-                ->add_fields(
-                    array(
-                        Field::make('text', 'school', __('School')),
-
-                    )
-                )
-                ->set_layout('tabbed-vertical')
-                ->set_header_template('<%- school  %>'),
-
-
-        )
-    );
 /*-----------------------------------------------------------------------------------*/
 /* Career Settings
 /*-----------------------------------------------------------------------------------*/
+
 Container::make('theme_options', __('Settings'))
     ->set_page_parent('jobsmanager')
     ->add_tab('General Settings', array(
