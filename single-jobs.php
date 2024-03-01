b491b0c6acd450e3828507155e47446ef337f130
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 21:26:26 +0800

    Update single-jobs.php

--- single-jobs.php
+++ single-jobs.php
@@ -54,7 +54,7 @@ $image = $JobsManager->get__post_meta('image');
                         </div>
                         <?php if ($image) { ?>
                             <div class="image-box">
-                                <img src="<?= wp_get_attachment_image_url($image, 'medium') ?>" alt="<?php the_title()?> image">
+                                <img src="<?= wp_get_attachment_image_url($image, 'medium') ?>" alt="<?php the_title() ?> image">
                             </div>
                         <?php } ?>
                     </div>
@@ -62,6 +62,36 @@ $image = $JobsManager->get__post_meta('image');
             </div>
             <div class="main-content">
                 <?php the_content() ?>
+            </div>
+            <div class="buttons">
+                <div class="row">
+                    <?php if ($JobsManager->jobs_single()) { ?>
+                        <div class="col">
+                            <div class="button-box">
+                                <a type="button" class="btn btn-link w-100 d-flex align-items-center" href="<?php the_permalink() ?>">
+                                    <span class="col text-center">LEARN MORE</span>
+                                    <span class="col-auto btn-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    <div class="col">
+                        <div class="button-box">
+                            <button type="button" class="apply-button btn btn-primary w-100 d-flex align-items-center" data-title="<?php the_title() ?>" data-bs-toggle="modal" data-bs-target="#applyModal">
+                                <span class="col text-center">APPLY FOR THIS POSITION</span>
+                                <span class="col-auto btn-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
