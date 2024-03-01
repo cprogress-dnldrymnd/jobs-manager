64e11638bc0363c8a097e303128e8d265af7c3a7
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 14:09:47 +0800

    Update admin-page.php

--- includes/admin-page.php
+++ includes/admin-page.php
@@ -4,6 +4,7 @@ class Admin_Page
     function __construct()
     {
         add_action('admin_menu', array($this, 'jobsmanager_setup_admin_page'));
+        add_action('parent_file', array($this, 'jobsmanager_highlight_taxonomy_parent_menu'));
     }
 
     function jobsmanager_setup_admin_page()
@@ -35,6 +36,15 @@ class Admin_Page
     function jobsmanager_setup_admin_page_init()
     {
         echo "<h1>Job Manager</h1>";
+    }
+
+    function jobsmanager_highlight_taxonomy_parent_menu($parent_file)
+    {
+        if (get_current_screen()->taxonomy == 'question_section') {
+            $parent_file = 'edit.php?post_type=quiz';
+        }
+
+        return $parent_file;
     }
 }
 
