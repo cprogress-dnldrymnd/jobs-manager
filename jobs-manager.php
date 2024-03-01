49e159b57a8ae92ab0cc7f735671caaab1ffbf02
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-03-01 13:03:43 +0800

    Update jobs-manager.php

--- jobs-manager.php
+++ jobs-manager.php
@@ -53,3 +53,12 @@ if (!class_exists('JobsManager')) {
 }
 
 new JobsManager();
+$Testimonials = new PostType();
+$Testimonials->name = 'Testimonials';
+$Testimonials->singular_name = 'Testimonial';
+$Testimonials->icon = 'dashicons-testimonial';
+$Testimonials->supports = array('title', 'revisions');
+$Testimonials->exclude_from_search = true;
+$Testimonials->publicly_queryable = false;
+$Testimonials->show_in_admin_bar = false;
+$Testimonials->has_archive = false;
